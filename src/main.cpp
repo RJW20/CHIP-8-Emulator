c00be80f39b84dfea0cc84484515c2f262a7c90d
RJW20 <robertjwatts84@outlook.com>
2024-11-29 16:21:05 +0000

    Write the entire main file/function

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,16 @@
-
+#include <iostream>
+#include "chip8.hpp"
 
 int main(int argc, char **argv) {
 
-    // Check for argument to run with
+    // Check for rom provided
+    if (argc != 2) {
+        std::cout << "Usage: chip8 <ROM file>" << std::endl;
+        return 1;
+    }
 
-    // Initialise Chip8 with the ROM
-
-    // Run
+    Chip8 chip8 = Chip8(argv[1]);
+    chip8.run();
 
     return 0;
 }
