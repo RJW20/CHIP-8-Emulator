5cd982ae45a5f2efe18998aee130372b08be3cce
RJW20 <robertjwatts84@outlook.com>
2024-11-30 16:26:24 +0000

    Implement the drawing of the display

--- src/chip8.cpp
+++ src/chip8.cpp
@@ -26,28 +26,61 @@ unsigned char fontset[80] = {
     0xF0, 0x80, 0xF0, 0x80, 0x80  //F
 };
 
-/* Initialise the Graphics, load the ROM into memory and set all components to
-their initial values. */
+/* Initialise SDL, load the ROM into memory and set all components to their
+initial values. */
 Chip8::Chip8(std::string file_path) {
-
-    // Initialise the graphics
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        std::cerr << "Failed to load SDL2 due to error:" << SDL_GetError() <<
-            std::endl;
-        exit(1);
-    };
-    window = SDL_CreateWindow(
-        "CHIP-8 Emulator", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-        64, 32, 0
-    );
-
+    initialise_SDL();
     rom_path = file_path;
     hard_reset();
 }
 
+// Destroy all SDL components
 Chip8::~Chip8() {
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
+}
+
+// Initialise all SDL components
+void Chip8::initialise_SDL() {
+
+    // Initialise SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError()
+            << std::endl;
+        exit(1);
+    };
+
+    // Create SDL window
+    window = SDL_CreateWindow(
+        "CHIP-8 Emulator", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        SCREEN_WIDTH, SCREEN_HEIGHT, 0
+    );
+    if (!window) {
+        std::cerr << "Window could not be created! SDL_Error: " <<
+            SDL_GetError() << std::endl;
+        exit(2);
+    }
+
+    // Create SDL renderer
+    renderer = SDL_CreateRenderer(window, -1, 0);
+    if (!renderer) {
+        std::cerr << "Renderer could not be created! SDL_Error: " <<
+            SDL_GetError() << std::endl;
+        exit(3);
+    }
+
+    // Create a texture to hold the pixel data
+    texture = SDL_CreateTexture(
+        renderer, SDL_PIXELFORMAT_RGB888, SDL_TEXTUREACCESS_STREAMING,
+        64, 32
+    );
+    if (!texture) {
+        std::cerr << "Texture could not be created! SDL_Error: " <<
+            SDL_GetError() << std::endl;
+        exit(4);
+    }
 }
 
 // Load the ROM into memory and soft reset
@@ -59,7 +92,7 @@ void Chip8::hard_reset() {
     std::ifstream file(rom_path, std::ios::binary | std::ios::ate);
     if (!file) {
         std::cerr << "Failed to open ROM: " << rom_path << std::endl;
-        exit(2);
+        exit(5);
     }
 
     // Get the file size
@@ -73,7 +106,7 @@ void Chip8::hard_reset() {
     char* buffer = new char[size];
     if (!file.read(buffer, size)) {
         std::cerr << "Failed to read file!" << std::endl;
-        exit(3);
+        exit(6);
     }
     file.close();
 
@@ -97,8 +130,10 @@ void Chip8::hard_reset() {
 // Set all components to their initial values
 void Chip8::soft_reset() {
 
-    for (int i = 0; i < 64 * 32; ++i) {
-        display[i] = 0;
+    for (int i = 0; i < 32; ++i) {
+        for (int j = 0; j < 64; ++j) {
+            display[i][j] = 0;
+        }
     }
     for (int i = 0; i < 16; ++i) {
         keys[i] = 0;
@@ -136,10 +171,16 @@ void Chip8::run() {
 
         render();
 
+        if (delay > 0) {
+            delay -=1;
+        }
+        if (sound > 0) {
+            sound -= 1;
+        }
+
         this_thread::sleep_until(next_frame);
         last_frame = next_frame;
         next_frame = next_frame + time_between_frames{1};
-
     }
 }
 
@@ -291,11 +332,15 @@ void Chip8::advance() {
                 
                 // 00E0 - clear screen
                 case 0x0000:
-                    for (int i = 0; i < 64 * 32; ++i) {
-                        display[i] = 0;
-                    }
-            }
 
+                    for (int i = 0; i < 32; ++i) {
+                        for (int j = 0; j < 64; ++j) {
+                            display[i][j] = 0;
+                        }
+                    }
+                    break;
+
+            }
             break;
 
         // 1NNN - jump to NNN
@@ -366,11 +411,11 @@ void Chip8::advance() {
 
                     if ((pixels >> (7 - col)) & 1 == 1) {
 
-                        if (display[64 * (y + row) + x + col]) {
+                        if (display[y + row][x + col]) {
                             V[0xF] = 1;
                         }
 
-                        display[64 * (y + row) + x + col] ^= 1;
+                        display[y + row][x + col] ^= 1;
 
                     }
                 }
@@ -391,6 +436,23 @@ void Chip8::advance() {
 // Draw the display to the window
 void Chip8::render() {
 
-    std::cout << "Frame drawn" << std::endl;
+    // Lock the texture to get direct access to its pixels
+    void* pixel_pointer;
+    int pitch;
+    if (SDL_LockTexture(texture, nullptr, &pixel_pointer, &pitch) != 0) {
+        std::cerr << "SDL_LockTexture failed: " << SDL_GetError() << std::endl;
+    }
 
+    // Modify the pixel data
+    uint32_t* pixel_buffer = static_cast<uint32_t*>(pixel_pointer);
+    for (int i = 0; i < 32; ++i) {
+        for (int j = 0; j < 64; ++j) {
+            pixel_buffer[i * 64 + j] = (0x00FFFFFF * display[i][j]) | 0xFF000000;
+        }
+    }
+
+    SDL_UnlockTexture(texture);
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+    SDL_RenderPresent(renderer);
 }
--- src/chip8.hpp
+++ src/chip8.hpp
@@ -6,6 +6,9 @@
 #include <stack>
 #include <SDL2/SDL.h>
 
+const int SCREEN_WIDTH = 1024;
+const int SCREEN_HEIGHT = 512;
+
 class Chip8 {
 public:
     Chip8(const std::string rom_path);
@@ -14,23 +17,27 @@ public:
     void run();
 
 private:
-    std::string rom_path;       // ROM location
+    std::string rom_path;           // ROM location
 
-    SDL_Window* window;         // SDL window
-    uint8_t display[64*32];     // Display buffer
-    uint8_t keys[16];           // Keypad
+    SDL_Window* window;             // SDL window
+    SDL_Renderer* renderer;         // SDL renderer
+    SDL_Texture* texture;           // SDL Texture
 
-    uint8_t memory[4096];       // Memory
-    uint16_t pc;                // Program counter
-    uint16_t I;                 // Index register
-    std::stack<uint8_t> stack;  // Call stack
-    uint8_t V[16];              // Variable registers
+    uint8_t display[32][64];        // Display buffer
+    uint8_t keys[16];               // Keypad
 
-    uint8_t delay;              // Delay timer
-    uint8_t sound;              // Sound timer
+    uint8_t memory[4096];           // Memory
+    uint16_t pc;                    // Program counter
+    uint16_t I;                     // Index register
+    std::stack<uint8_t> stack;      // Call stack
+    uint8_t V[16];                  // Variable registers
 
-    const int APF = 11;         // Advances per frame
+    uint8_t delay;                  // Delay timer
+    uint8_t sound;                  // Sound timer
 
+    const int APF = 11;             // Advances per frame
+
+    void initialise_SDL();
     void hard_reset();
     void soft_reset();
 
